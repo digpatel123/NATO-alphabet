51f1b6a81d52e23fd541eb8c883f7018357d505a
digpatel123 <digpatel123@gmail.com>
2023-05-23 10:26:29 +0100

    Completed.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ alpha_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = list(input("Please enter a word for which you want the NATO alphabets: ").upper())
+user_input = list(input("Please enter a word or name for which you want the NATO alphabets: ").upper())
 
 nato_codes = [alpha_dict[let] for let in user_input]
 
