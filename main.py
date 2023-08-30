9f5efba45fe3dcbe6841e88f7b3e43bcc9a74577
digpatel123 <59032434+digpatel123@users.noreply.github.com>
2023-08-30 19:19:12 +0100

    Implemented exception handling.

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ alpha_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = list(input("Please enter a word or name for which you want the NATO alphabets: ").upper())
+def create_nato():
+    user_input = list(input("Please enter a word or name for which you want the NATO alphabets: ").upper())
 
-nato_codes = [alpha_dict[let] for let in user_input]
-
-print(nato_codes)
+    try:
+        nato_codes = [alpha_dict[let] for let in user_input]
+    except KeyError:
+        print("Please only enter letters of the alphabets.")
+        create_nato()
+    else:
+        print(nato_codes)
+create_nato()
